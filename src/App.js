22b91965c32b56ea2f348028990a01009dc56ad6
pranali-hkb <pranalibos.hkb@gmail.com>
2023-06-15 17:46:41 +0530

    some changes in app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       {/* <LoginPage/> */}
+      {/* <LoginPage/> */}
       <BrowserRouter>
         {/* <LoginPage /> */}
         <HomePage/>
