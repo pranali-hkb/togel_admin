10e54805eee747930cb4ce504a4412be9d329d5f
pranali-hkb <pranalibos.hkb@gmail.com>
2023-06-23 15:16:42 +0530

    settig popup done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
-import LoginPage from "./components/LoginPage/LoginPage";
-import Test from "./components/Test/Test";
-import TurnoverReport from "./components/TurnoverReport/TurnoverReport";
-import WinLossReport from "./components/WinLossReport/WinLossReport";
-import TransactionReport from "./components/TransactionReport/TransactionReport";
-
 
 
 function App() {
   return (
     <div className="App">
-  {/* <TurnoverReport/>
-     */}
-     {/* <TransactionReport/> */}
-     {/* <WinLossReport/> */}
+    
       <BrowserRouter>
-<HomePage/>
+        {/* <HomePage /> */}
       </BrowserRouter>
     </div>
   );
