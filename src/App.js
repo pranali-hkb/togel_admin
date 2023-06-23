983cb51a668c877ebc635a06847bdcf79559c0c1
pranali-hkb <pranalibos.hkb@gmail.com>
2023-06-23 18:04:41 +0530

    update user profile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom";
 import HomePage from "./components/HomePage/HomePage";
+import UpdateProfile from "./components/UpdateProfile/UpdateProfile";
 
 
 function App() {
   return (
     <div className="App">
-    
+    <UpdateProfile/>
       <BrowserRouter>
         {/* <HomePage /> */}
       </BrowserRouter>
--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -20,9 +20,6 @@ import Checkbox from "@mui/material/Checkbox";
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 
-
-
-
 const Item = styled(Paper)(({ theme }) => ({
   // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
